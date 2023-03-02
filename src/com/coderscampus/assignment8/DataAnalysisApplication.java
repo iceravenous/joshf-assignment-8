0ef6d4216eb8d83933110d69a6d100472d286b36
iceravenous <67291394+iceravenous@users.noreply.github.com>
2023-03-01 22:51:26 -0500

    ver 0301

--- src/com/coderscampus/assignment8/DataAnalysisApplication.java
+++ src/com/coderscampus/assignment8/DataAnalysisApplication.java
@@ -2,43 +2,70 @@ package com.coderscampus.assignment8;
 
 
 import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
+//import java.util.concurrent.Future;
+//import java.util.stream.Collectors;
+
+
 
 public class DataAnalysisApplication {
+	
+	static class Task extends Thread {
+		
+		Assignment8 assignment = new Assignment8();
+		private int number;
+		
+		public void Task(int number) {
+			this.number = number;
+		}
+		
+		public void run() {
+			
+			List<Integer> numbersList = new ArrayList<>();
+			for (int i=0; i<5; i++) {
+				numbersList.addAll(assignment.getNumbers());
+			}	
+			//end of method
+		}		
+	}
+
 
 	public static void main(String[] args) {
-		//DataService.getDataWrong();
-//		List<Integer> numbers = DataService.getList();
-//		System.out.println(numbers);
+//		Assignment8 assignment = new Assignment8();
 		
-//		DataService.getData();
 		
-		CompletableFuture<List<Integer>> completableList = new CompletableFuture<List<Integer>>();
-		List<CompletableFuture<Integer>> tasks = new ArrayList<>();
-		ExecutorService pool = Executors.newCachedThreadPool();
-//		ExecutorService boundTask2 = Executors.newFixedThreadPool(10);
+		//CompletableFuture<List<Integer>> completableList = new CompletableFuture<List<Integer>>();
+//		ExecutorService pool = Executors.newCachedThreadPool();
+		ExecutorService boundTask = Executors.newFixedThreadPool(10);
+//			completableList = CompletableFuture.supplyAsync(() -> new DataService(), pool )
+//					.thenApplyAsync(dataService -> dataService.getData(), pool);
+//			completableList = CompletableFuture.thenApplyAsync(data -> data.getData(), pool);
+//		System.out.println(completableList);
+//
 		
-				
-
-			//List<Integer> numbersList =
-//		for (int i=0; i>10;i++ ) {
-			completableList = CompletableFuture.supplyAsync(() -> new DataService(), pool )
-					.thenApplyAsync(dataService -> dataService.getData(), pool);
-//					.thenAcceptAsync(dto -> System.out.println(dto), boundTask);
+//		for (int i=0; i<20; i++) {
+//			pool.execute((Runnable) getData(assignment));
+//		
 //		}
-
-		System.out.println(completableList);
+//		pool.execute(new Task()); 
+		boundTask.execute(new Task());
 		
-//		Map<Object, Long> counts = completableList.stream()
-//				.collect(Collectors.groupingBy(e->e, Collectors.counting()));
-//		System.out.println(counts);
-//	
+		
+//		public List<Integer> getData(Assignment8 assignment) {
+//			List<Integer> numbersList = new ArrayList<>();
+//			for (int i=0; i<20; i++) {
+//				numbersList.addAll(assignment.getNumbers());
+//			}
+//			return numbersList;
+//
+//		}
 	}
+	
+
 
 }
