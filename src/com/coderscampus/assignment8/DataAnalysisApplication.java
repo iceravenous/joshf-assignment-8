2a598830567340a8c7830f2aace38bb1f7946d3e
iceravenous <67291394+iceravenous@users.noreply.github.com>
2023-03-02 22:48:22 -0500

    ver 0302

--- src/com/coderscampus/assignment8/DataAnalysisApplication.java
+++ src/com/coderscampus/assignment8/DataAnalysisApplication.java
@@ -4,10 +4,12 @@ package com.coderscampus.assignment8;
 import java.util.ArrayList;
 //import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 //import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 //import java.util.concurrent.Future;
 //import java.util.stream.Collectors;
 
@@ -15,55 +17,32 @@ import java.util.concurrent.Executors;
 
 public class DataAnalysisApplication {
 	
-	static class Task extends Thread {
-		
+	public static void main(String[] args) {
 		Assignment8 assignment = new Assignment8();
-		private int number;
 		
-		public void Task(int number) {
-			this.number = number;
+		
+		CompletableFuture<List<Integer>> completableList = new CompletableFuture<List<Integer>>();
+		ExecutorService pool = Executors.newCachedThreadPool();
+
+		for (int i=0; i < 10; i++) {
+			completableList = CompletableFuture.supplyAsync(() -> assignment.getNumbers(), pool);
+			
+		}
+		List<Integer> finishList = new ArrayList<Integer>();
+		finishList = completableList.join();
+		
+
+
+		Map<Object, Long> counts = finishList.stream()
+				.collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+		System.out.println("\nSize of list: " + finishList.size());
+		System.out.println("\n===================\n");
+		for (Map.Entry<Object, Long> entry : counts.entrySet()) {
+			System.out.print(entry.getKey() + "=" + entry.getValue() + ", ");
+			
 		}
 		
-		public void run() {
-			
-			List<Integer> numbersList = new ArrayList<>();
-			for (int i=0; i<5; i++) {
-				numbersList.addAll(assignment.getNumbers());
-			}	
-			//end of method
-		}		
-	}
-
-
-	public static void main(String[] args) {
-//		Assignment8 assignment = new Assignment8();
-		
-		
-		//CompletableFuture<List<Integer>> completableList = new CompletableFuture<List<Integer>>();
-//		ExecutorService pool = Executors.newCachedThreadPool();
-		ExecutorService boundTask = Executors.newFixedThreadPool(10);
-//			completableList = CompletableFuture.supplyAsync(() -> new DataService(), pool )
-//					.thenApplyAsync(dataService -> dataService.getData(), pool);
-//			completableList = CompletableFuture.thenApplyAsync(data -> data.getData(), pool);
-//		System.out.println(completableList);
-//
-		
-//		for (int i=0; i<20; i++) {
-//			pool.execute((Runnable) getData(assignment));
-//		
-//		}
-//		pool.execute(new Task()); 
-		boundTask.execute(new Task());
-		
-		
-//		public List<Integer> getData(Assignment8 assignment) {
-//			List<Integer> numbersList = new ArrayList<>();
-//			for (int i=0; i<20; i++) {
-//				numbersList.addAll(assignment.getNumbers());
-//			}
-//			return numbersList;
-//
-//		}
+	
 	}
 	
 
