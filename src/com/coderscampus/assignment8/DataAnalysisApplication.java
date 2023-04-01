6d81e09259ef6e3e77d20320c97c3761ccdc8684
iceravenous <joshuafarlow@gmail.com>
2023-04-01 14:57:15 -0400

    v0401beta

--- src/com/coderscampus/assignment8/DataAnalysisApplication.java
+++ src/com/coderscampus/assignment8/DataAnalysisApplication.java
@@ -2,16 +2,13 @@ package com.coderscampus.assignment8;
 
 
 import java.util.ArrayList;
-//import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-//import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
-//import java.util.concurrent.Future;
-//import java.util.stream.Collectors;
+
 
 
 
@@ -20,26 +17,8 @@ public class DataAnalysisApplication {
 	public static void main(String[] args) {
 		Assignment8 assignment = new Assignment8();
 		List<Integer> finishList = new ArrayList<Integer>();
-//		List<CompletableFuture<Integer>> resultList = new ArrayList<>();
-		
-//		int batchSize = 1000;
-//		int startIndex = 0;
-//		while (true) {
-//			List<Integer> subList = assignment.getNumb);
-//		}
-//		
-		
-//////		List<CompletableFuture<Integer>> futureList = Arrays.asList(
-////				CompletableFuture.supplyAsync(() -> assignment.getNumbers())
-////				);
-////		CompletableFuture<List<Integer>> completableListTemp = new CompletableFuture<List<Integer>>();
-//		
-//		
-//		
-//	CompletableFuture<List<Integer>> completableList = new CompletableFuture<List<Integer>>();
 		ExecutorService pool = Executors.newCachedThreadPool();
 		
-//		completableList = CompletableFuture.supplyAsync(() -> assignment.getNumbers(), pool);
 List<CompletableFuture<List<Integer>>> completableList = new ArrayList<>();
 		
 		List<Integer> outputList = new ArrayList<>();
@@ -66,25 +45,13 @@ List<CompletableFuture<List<Integer>>> completableList = new ArrayList<>();
 				.collect(Collectors.toList());
 		
 
-//		 
+		 
 		Map<Object, Long> counts = finishList.stream()
 				.collect(Collectors.groupingBy(e -> e, Collectors.counting()));
 		
-//		String countsString = counts.keySet().stream()
-//				.map(key -> key + "=" + map.get(key))
-//				.collect(Collectors.joining(", "));
 		String countsString = counts.toString();
 		countsString = countsString.substring(1, countsString.length() - 1); 
-		
-//		System.out.println("\nSize of list: " + finishList.size());
-//		System.out.println("\n===================\n");
-//		for (Map.Entry<Object, Long> entry : counts.entrySet()) {
-//			System.out.print(entry.getKey() + "=" + entry.getValue() + ", ");
-//			countsString.concat(entry.getKey() + "=" + entry.getValue() + ", ");			
-//		}
-//		String finalCounts = countsString.substring(0,countsString.length()-1);
-//		
-//	System.out.println(finalCounts);
+
 		System.out.println(countsString);
 				
 
